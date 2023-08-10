e25a27c3e215cd37bd4aaf12a6ff754fbe32ba25
Berkay <berkaysutlu94@gmail.com>
2023-08-10 11:53:38 +0300

    Initial Commit

---
+++ BlogApp/BlogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogAppApp.swift
+//  BlogApp
+//
+//  Created by Berkay Sutlu on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
